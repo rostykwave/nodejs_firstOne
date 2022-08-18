fd6967cf0cc5bf035e4e0da8fca19e01e1995505
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-08-18 20:11:13 +0300

    2.9 start

--- index.js
+++ index.js
@@ -1,68 +1,17 @@
 const express = require("express");
 const morgan = require("morgan");
-const got = require("got");
-const cors = require("cors");
-const app = express();
 require("dotenv").config();
 
-// const { router } = require("./booksRouter");
+const app = express();
+
+const { router } = require("./booksRouter");
+
 const PORT = process.env.PORT || 8081;
-// const PORT = process.env.PORT || 8081;//without dotenv
-// const PORT = 8081;
-const thirdPartyBaseUrl = "http://api.weatherbit.io/v2.0/current";
-const thirdPartyApiKey = process.env.WEATHER_API_KEY;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
 app.use(morgan("tiny"));
 app.use(cors());
 // app.use("/api", router);
-
-// app.use((req, res, next) => {
-//   console.log(`${req.method} ${req.originalUrl} ${new Date().toISOString()}`);
-//   next();
-// });
-
-//http://api.weatherbit.io/v2.0/current?key=ea41bc3d393b41638cdd827b88acf3ef&lat=50.427107&lon=30.567437
-
-app.get("/api/weather", async (req, res) => {
-  try {
-    const { latitude, longitude } = req.query;
-
-    if (!latitude) {
-      return res
-        .status(400)
-        .json({ message: "latitude parameter is mandatory" });
-    }
-    if (!longitude) {
-      return res
-        .status(400)
-        .json({ message: "longitude parameter is mandatory" });
-    }
-
-    const response = await got(thirdPartyBaseUrl, {
-      searchParams: {
-        key: thirdPartyApiKey,
-        lat: latitude,
-        lon: longitude,
-        // lat: "50.427107",
-        // lon: "30.567437",
-      },
-      responseType: "json",
-    });
-    const [weatherData] = response.body.data;
-    const {
-      city_name,
-      weather: { description },
-      temp,
-    } = weatherData;
-
-    res.json({ city_name, description, temp });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
 
 app.listen(PORT, (err) => {
   if (err) {
