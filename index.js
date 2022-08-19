9bc0f42f9355e1e906b0ef30b4da1741b96f1c54
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-08-19 18:39:37 +0300

    c

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const express = require("express");
-const morgan = require("morgan");
-require("dotenv").config();
+const express = require('express');
+const morgan = require('morgan');
+require('dotenv').config();
 
 const app = express();
 
-const { postsRouter } = require("./routers/postsRouter");
+const { postsRouter } = require('./routers/postsRouter');
 
 const PORT = process.env.PORT || 8081;
 
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(morgan('dev'));
 
-app.use("/api/posts", postsRouter);
+app.use('/api/posts', postsRouter);
 
-app.listen(PORT, (err) => {
+app.listen(PORT, err => {
   if (err) {
-    console.error("Error at a servers launch: ", err);
+    console.error('Error at a servers launch: ', err);
   }
   console.log(`Server works at port ${PORT}!`);
 });
