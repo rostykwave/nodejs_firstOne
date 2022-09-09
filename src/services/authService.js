f1ff72e682051cc037be9ead55e40b09508bd0fc
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-09-09 22:01:10 +0300

    sendgrid

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,5 +1,9 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const sgMail = require('@sendgrid/mail');
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 const { User } = require('../db/userModel');
 const { NotAuthorizedError } = require('../helpers/errors');
 
@@ -7,6 +11,16 @@ const registration = async (email, password) => {
   const user = new User({ email, password });
 
   await user.save();
+
+  const msg = {
+    to: email,
+    from: 'jakeko2012@gmail.com',
+    subject: 'Thank you for registration!',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<h1>and easy to do anywhere, even with Node.js</h1>',
+  };
+
+  await sgMail.send(msg);
 };
 
 const login = async (email, password) => {
